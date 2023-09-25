c4c79023b546a9ddadb7873fc4abb5c5f9bcecb8
ndanHD <86829808+ndanHD@users.noreply.github.com>
2023-09-25 09:15:10 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,8 +40,7 @@ $(document).ready(function () {
 
     Swal.fire({
       title: 'Hubungi Kami',
-      html:
-        `<form>
+      html: `<form>
         <div class="mb-3">
           <select class="form-select" id="jenis" required>
             <option value="1" selected>Informasi Lebih Lanjut</option>
@@ -55,30 +54,35 @@ $(document).ready(function () {
       confirmButtonText: 'Kirim Pesan',
       focusConfirm: false,
       preConfirm: () => {
-        const jenis = Swal.getPopup().querySelector('#jenis').value
-        const pesan = Swal.getPopup().querySelector('#pesan').value
+        const jenis = Swal.getPopup().querySelector('#jenis').value;
+        const pesan = Swal.getPopup().querySelector('#pesan').value;
         var ndate = new Date();
         var hours = ndate.getHours();
         var sapaan = hours < 12 ? 'Selamat Pagi' : hours < 18 ? 'Selamat Siang' : 'Selamat Malam';
-        var gabungan = "";
+        var gabungan = '';
 
         // cek pesan kosong
-        if (pesan === "") {
-          Swal.showValidationMessage(`Pesan Anda masih kosong`)
+        if (pesan === '') {
+          Swal.showValidationMessage(`Pesan Anda masih kosong`);
         }
 
-        if (jenis == "1") {
+        if (jenis == '1') {
           gabungan = `${sapaan} Kakak admin *Milinkbeauty*, Saya ingin bertanya *informasi lebih lanjut* mengenai :%0a${pesan}`;
         } else {
           gabungan = `${sapaan} Kakak admin *Milinkbeauty*, Saya ingin *membuat jadwal* mengenai :%0a ${pesan}`;
         }
 
         // Buat message
-        return { gabungan: gabungan }
-      }
+        return { gabungan: gabungan };
+      },
     }).then((result) => {
-      linkWa(result.value.gabungan)
-    })
+      linkWa(result.value.gabungan);
+    });
   });
-
 });
+
+// script by hawa
+function toggleAnswer(element) {
+  var answer = element.nextElementSibling;
+  answer.classList.toggle('active');
+}
